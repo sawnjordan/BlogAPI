6c2064d6c998676b4d78ea46173e7bbe24b997a3
sawnjordan <sawnjordan@gmail.com>
2023-06-21 20:42:16 +0545

    Added login route

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -41,7 +41,30 @@ const registerUser = async (req, res, next) => {
     console.log(err);
     return res.status(400).json({ msg: err?.message });
   }
-  res.status(201).json({ user });
+  res.status(201).json({ msg: "User Created", user });
 };
 
-module.exports = { getAllUsers, registerUser };
+const userLogin = async (req, res, next) => {
+  const { email, password } = req.body;
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ msg: err?.message });
+  }
+
+  if (!existingUser) {
+    return res
+      .status(404)
+      .json({ msg: "Cann't find the user with this email." });
+  }
+
+  const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+  if (!isPasswordCorrect) {
+    return res.status(400).json({ msg: "Incorrect Password" });
+  }
+  return res.status(200).json({ msg: "Login successful" });
+};
+
+module.exports = { getAllUsers, registerUser, userLogin };
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   getAllUsers,
   registerUser,
+  userLogin,
 } = require("../controllers/users.controller");
 
 const router = express.Router();
 
 router.get("/", getAllUsers);
 router.post("/register", registerUser);
+router.post("/login", userLogin);
 
 module.exports = router;
